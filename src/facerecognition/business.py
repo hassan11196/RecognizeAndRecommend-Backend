907bf511a19f8d337d85b062abea71919bedeab5
nomananjum <nomananjum420@gmail.com>
2020-12-21 16:52:07 +0500

    Return format changed in image det

--- src/facerecognition/business.py
+++ src/facerecognition/business.py
@@ -121,18 +121,42 @@ def label_image(request):
                 name = known_face_names[best_match_index]
 
         face_names.append(name)
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+    # for (top, right, bottom, left), name in zip(face_locations, face_names):
+    #     # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+    #     top *= 4
+    #     right *= 4
+    #     bottom *= 4
+    #     left *= 4
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+    #     # Draw a box around the face
+    #     cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 3.0, (255, 255, 255), 4)
-    return {"status":True,"message":"Face Has Been Recognized","label":face_names}       
+    #     # Draw a label with a name below the face
+    #     cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+    #     font = cv2.FONT_HERSHEY_DUPLEX
+    #     cv2.putText(frame, name, (left + 6, bottom - 6), font, 3.0, (255, 255, 255), 4)
+    if len(face_names) == 1:
+        if(face_names[0] == 'Unknown'):
+            return {"status": True,
+            "message": "Image Received.",
+            "data": {
+                "confidence": 0,
+                "name": face_names,
+                "auth_token": None
+            }}
+        else:
+            return {"status": True,
+            "message": "Image Received.",
+            "data": {
+                "confidence": 100,
+                "name": face_names,
+                "auth_token": None
+            }}
+
+    return {"status": True,
+            "message": "Image Received With Multiple Faces.",
+            "data": {
+                "confidence": 100,
+                "name": face_names,
+                "auth_token": None
+            }}       
