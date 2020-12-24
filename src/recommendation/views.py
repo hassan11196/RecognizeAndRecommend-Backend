1c2554ab1690fd91fb79a8354036057bbd3021a7
hassan11196 <hassan11196@hotmail.com>
2020-12-24 21:38:45 +0500

    Db Refresh

--- src/recommendation/views.py
+++ src/recommendation/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from src.recommendation.serializers import ProductPriceSerializer, CategorySerializer, ProductImageSerializer, ProductFeatureBulletSerializer, ProductReviewMetaDataSerializer, ProductVariantSerializer, ProductSerializer
-from src.recommendation.models import ProductPrice, Category, ProductImage, ProductFeatureBullet, ProductReviewMetaData, ProductVariant, Product
+from src.recommendation.serializers import ProductPriceSerializer, CategorySerializer, ProductImageSerializer, ProductFeatureBulletSerializer, ProductReviewMetaDataSerializer, ProductVariantSerializer, ProductSerializer, ProductReviewSerializer
+from src.recommendation.models import ProductPrice, Category, ProductImage, ProductFeatureBullet, ProductReviewMetaData, ProductVariant, Product, ProductReview
 
 
 class ProductPriceViewSet(ViewSet):
@@ -280,6 +280,46 @@ class ProductViewSet(ViewSet):
         try:
             item = Product.objects.get(pk=pk)
         except Product.DoesNotExist:
+            return Response(status=404)
+        item.delete()
+        return Response(status=204)
+
+
+class ProductReviewViewSet(ViewSet):
+
+    def list(self, request):
+        queryset = ProductReview.objects.order_by('pk')
+        serializer = ProductReviewSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ProductReviewSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+    def retrieve(self, request, pk=None):
+        queryset = ProductReview.objects.all()
+        item = get_object_or_404(queryset, pk=pk)
+        serializer = ProductReviewSerializer(item)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        try:
+            item = ProductReview.objects.get(pk=pk)
+        except ProductReview.DoesNotExist:
+            return Response(status=404)
+        serializer = ProductReviewSerializer(item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    def destroy(self, request, pk=None):
+        try:
+            item = ProductReview.objects.get(pk=pk)
+        except ProductReview.DoesNotExist:
             return Response(status=404)
         item.delete()
         return Response(status=204)
